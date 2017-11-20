da6dd53da89b1c1873bbc7107f0c266fb6bc569c
Taylor Short <treddshort@gmail.com>
2017-11-20 10:28:46 -0800

    added </br> tag and altered font-size

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,23 +7,23 @@ $(document).ready(function() {
 
     var inputNum = $("input#inputNum").val();
 
-    function someNum(inputNum) {
+    someNum = inputNum => {
 
-    if (inputNum % 15 === 0){
-      return 'pingpong';
-    } else if (inputNum % 5 === 0) {
-      return 'pong';
-    } else if (inputNum % 3 === 0) {
-      return 'ping';
-    } else {
-      return inputNum;
-     }
+      if (inputNum % 15 === 0){
+        return 'pingpong';
+      } else if (inputNum % 5 === 0) {
+        return 'pong';
+      } else if (inputNum % 3 === 0) {
+        return 'ping';
+      } else {
+        return inputNum;
+      }
     }
 
 
     for (var i = 1; i <= inputNum; i++){
-    $("#sentenceTwo").append(someNum(i) + "<br />");
-}
+      $("#sentenceTwo").append(someNum(i) + "<br />");
+    }
     event.preventDefault();
 
   });
